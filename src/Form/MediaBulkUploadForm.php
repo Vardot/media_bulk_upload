4fa56ac87157a60fd71ad27645374c4445257aae
HJ-b <HJ-b@1010950.no-reply.drupal.org>
2022-04-06 09:34:16 +0200

    Issue #3055811 by hj-b: The field value for "media_bulk_upload_information" is not translatable

--- src/Form/MediaBulkUploadForm.php
+++ src/Form/MediaBulkUploadForm.php
@@ -201,13 +201,10 @@ class MediaBulkUploadForm extends FormBase {
 
     $form['information_wrapper']['warning'] = [
       '#type' => 'html_tag',
-      '#tag' => 'span',
+      '#tag' => 'p',
       '#id' => 'media_bulk_upload_information',
       '#name' => 'media_bulk_upload_information',
-      '#value' => '<p>Please be
-        aware that if file extensions overlap between the media types that are
-        available in this upload form, that the media entity will be assigned
-        automatically to one of these types.</p>',
+      '#value' => $this->t('Please be aware that if file extensions overlap between the media types that are available in this upload form, that the media entity will be assigned automatically to one of these types.'),
     ];
 
     $form['dropzonejs'] = [
