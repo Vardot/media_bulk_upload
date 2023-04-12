bf56cccbc8540afd8ed12318778e87d66a4d4bc0
Mrinalini Kumari <47251-mrinalini9@users.noreply.drupalcode.org>
2023-04-11 21:36:17 -0700

    Issue #3324771 by mrinalini9, sudesh.solaskar, Kristen Pol: Validation missing when empty bulk media upload form submitted

--- src/Form/MediaBulkUploadForm.php
+++ src/Form/MediaBulkUploadForm.php
@@ -599,4 +599,19 @@ class MediaBulkUploadForm extends FormBase {
     }
     return $this;
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    // When using dropzonejs, check at least one file was provided.
+    if (\Drupal::service('module_handler')->moduleExists('media_bulk_upload_dropzonejs')) {
+      $dropzonejs = $form_state->getValue('dropzonejs');
+      if (empty($dropzonejs['uploaded_files'])) {
+        $form_state->setErrorByName('dropzonejs', $this->t('No media files have been provided.'));
+        return;
+      }
+    }
+  }
+
 }
